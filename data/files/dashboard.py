70c2040022fe7402dfbfee078501ff5644575b92
Raman Deep <rd966443@gmail.com>
2026-04-05 13:53:40 +0530

    dashboard file chnged

--- data/files/dashboard.py
+++ data/files/dashboard.py
@@ -413,9 +413,10 @@ elif page=="✔️Prediction":
         st.subheader("📌 Saved Customer Data")
         st.write(st.session_state["input_data"])
         if st.button("🚀 Predict Churn"):
-            # Convert to DataFrame
+            input_df = pd.DataFrame([st.session_state["input_data"]])
+            # Align with model features
             input_df = input_df.reindex(columns=model.feature_names_in_, fill_value=0)
-            
+        
             with st.spinner("🔄 Processing..."):
                 prediction = model.predict(input_df)[0]
                 probability = model.predict_proba(input_df)[0][1]
